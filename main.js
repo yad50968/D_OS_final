a370d697be803a26b2b486d282839cfcefc3acf9
yad50968 <yad50968@gmail.com>
2021-06-03 22:37:03 +0800

    update

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 
 var helper = require("./helper");
 var axios = require('axios');
-var web3Func = require('web3Func');
+var web3Func = require('./web3Func');
 
-var apiUrl = "https://firebasestorage.googleapis.com/v0/b/nft-ipfs.appspot.com/o/userdata.json?alt=media&token=3fdc509b-cfac-448f-b3dc-48ead043c05e&fbclid=IwAR0J8v9ALLGV-qFVnRTzTVxFCtV4CAS9wr1_O1gEdlhfR8VehKTQJhVrYjM";
+//var apiUrl = "https://firebasestorage.googleapis.com/v0/b/nft-ipfs.appspot.com/o/userdata.json?alt=media&token=3fdc509b-cfac-448f-b3dc-48ead043c05e&fbclid=IwAR0J8v9ALLGV-qFVnRTzTVxFCtV4CAS9wr1_O1gEdlhfR8VehKTQJhVrYjM";
+
+var apiUrl = "http://127.0.0.1:3000/";
 
 
 async function main() {
@@ -11,6 +13,8 @@ async function main() {
     let response = await axios.get(apiUrl);
     let json_data = response['data'];
     if (!helper.isEmptyObject(json_data)) {
+        
+        console.log(json_data);
         json_data.forEach(async jd => {
             let jdTurn = jd["turn"];
             if (jdTurn === 2) {
@@ -19,8 +23,9 @@ async function main() {
                 let jdName = jd["tokennam"];
                 let jdSymbol = jd["nftnam"];
                 let jdUri = jd["uri"];
-                let sc_tx = await web3Func.deploy_sc(jdName, jdSymbol, jdSK);
-                let mint_tx = await web3Func.mintToken(sc_tx, jdAddress, jdUri, jdSK);
+                let sc_tx = await web3Func.deploySC_Mint(jdName, jdSymbol, jdAddress, jdSK);
+                //let mint_tx = await web3Func.mintToken(sc_tx, jdAddress, jdUri, jdSK);
+                console.log('fff');
             }
         })
     } else {
@@ -29,4 +34,4 @@ async function main() {
 }
 //request_for_data();
 main();
-//setInterval(request_for_data, 60000);
+//setInterval(main, 1000);
--- web3Func.js
+++ web3Func.js
@@ -1,18 +1,35 @@
 const Web3 = require('web3');
-const rpcURL = 'http://127.0.0.1:7545';
+const fs = require('fs');
+const rpcURL = 'http://127.0.0.1:8545';
 const web3 = new Web3(rpcURL);
-const abi = ;
-const bytecode= ;
-const deploySC = (jdName, jdSymbol, jdSK) => {
-    let deploy_contract = new web3.eth.Contract(JSON.parse(abi));
-}
+const abi = fs.readFileSync('./abi.json').toString();
+const bytecode = fs.readFileSync('./bytecode.bin').toString();
 
-const deploySC = () => {
+
+
+const deploySC_Mint = async (jdName, jdSymbol, jdAddress, jdSK) => {
+
+    console.log('enter deploySC');
+
+    let contract = new web3.eth.Contract(JSON.parse(abi));
+
+    let deploy_tx = await contract.deploy({data: "0x" + bytecode, arguments: [jdName, jdSymbol]});
+
+    let gas = await deploy_tx.estimateGas({from: jdAddress});
+    let options = {
+        to  : deploy_tx._parent._address,
+        data: deploy_tx.encodeABI(),
+        gas : gas
+    };
+
+    let signedTransaction = await web3.eth.accounts.signTransaction(options, jdSK);
+    let deploy_address = await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
+
+    
 
 }
 
 module.exports =
 {
-    deploySC,
-    mintToken
+    deploySC_Mint
 }
